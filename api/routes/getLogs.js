82bd1959f96fd8e770d497434d183ded5e44078a
SuhEugene <wasddsawwsasd@gmail.com>
2020-11-01 19:00:48 +0300

    Too much info for noname...

--- api/routes/getLogs.js
+++ api/routes/getLogs.js
@@ -9,10 +9,10 @@ module.exports = (req) => {
     }, [], {
       limit: 30,
       sort:{ timestamp: -1 }
-    }).populate("fromApp")
-      .populate("fromUser")
-      .populate("toApp")
-      .populate("toUser");
+    }).populate("fromApp", "name _id shortname image level")
+      .populate("toApp",   "name _id shortname image level")
+      .populate("fromUser", "_id username status id uuid")
+      .populate("toUser", "_id username status id uuid");
     send(logs_data);
   })
 }
