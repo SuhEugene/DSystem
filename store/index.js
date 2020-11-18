d0ae119eaf3b7dca54e2d917a47b4577c4f6da24
SuhEugene <wasddsawwsasd@gmail.com>
2020-11-18 14:59:32 +0300

    Dark theme fixes

--- store/index.js
+++ store/index.js
@@ -7,5 +7,5 @@ export const mutations = {
   setLogs (state, logs) { state.logs = logs },
   setBal (state, bal)   { state.auth.user.balance = bal },
   changeTheme (state)   { state.dark = !state.dark; localStorage.setItem("dark", state.dark); },
-  setTheme (state, theme) { state.dark = theme }
+  setTheme (state, theme) { state.dark = theme; localStorage.setItem("dark", theme); }
 };
