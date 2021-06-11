aef086c51414b64848f3300d1c2b1be3a7dcee10
SuhEugene <wasddsawwsasd@gmail.com>
2021-06-11 03:48:09 +0300

    New domain and scope for test bot

--- bot/h.js
+++ bot/h.js
@@ -18,12 +18,12 @@ app.use(bodyParser.json());
 app.get("/success", async (req, res) => {
   if (!req.query.code) return res.send("Мда");
     try {
-      const r = await fetch("https://oauth.di-api.net.ru/token", {
+      const r = await fetch("https://oauth.drom.one/token", {
         method: "POST",
         body: JSON.stringify({
           client_id: "5fd13b3c45a6ee51f1a6f4d3",
           client_secret: "2967104",
-          redirect_uri: "https://hh.di-api.net.ru/success",
+          redirect_uri: "http://hh.di-api.net.ru/success",
           scope: req.query.scope.split(/ +/),
           code: req.query.code
         }),
@@ -33,7 +33,7 @@ app.get("/success", async (req, res) => {
       console.log("дата", data);
       console.log("асыс", data.access_token);
       if (!data.access_token) return res.send("Ашипка получения асыс, видимо код оноразовый")
-      const r2 = await fetch("https://oauth.di-api.net.ru/api/user", {
+      const r2 = await fetch("https://oauth.drom.one/api/user", {
         headers: { Authorization: `Bearer ${data.access_token}`}
       });
       let user = await r2.json();
@@ -44,8 +44,12 @@ app.get("/success", async (req, res) => {
       if (allScopes.includes("data"))
         text.push(`Ты - <code>${user.username}</code> с discord id <code>${user.id}</code> и uuid <code>${user.uuid}</code><br><br>Твой id внутри системы: <code>${user._id}</code>`);
 
-      if (allScopes.includes("balance"))
-        text.push(`Твой баланс: <code>${user.balance} АР</code>`);
+      if (allScopes.includes("cards")) {
+        for (let card of user.cards) {
+          text.push(`Карта <code>${card.id}</code>: <code>${card.text}</code>/<code>${card.balance} АР</code>`);
+        }
+      }
+        
 
       const roles = ["никем", "обычным юзером", "банкиром", "модером", "админом"];
       if (allScopes.includes("status")) text.push(`Статус у тебя: <code>"${user.status}"</code>. Мда...`);
@@ -65,7 +69,7 @@ app.get('/buy', (req, res) => {
   res.send(`
     <body style='height:100vh; width:100%; display:flex; flex-direction:column; align-items:center; justify-content:center;'>
       <h1>PLEASE</h1>
-      <p>BUY THIS SHIT!!! <a href='https://main.di-api.net.ru/app/suheugene/8?text=Шалкер&uid=123'>CLICK</a></p>
+      <p>BUY THIS SHIT!!! <a href='https://drom.one/app/suheugene/8?text=Шалкер&uid=123'>CLICK</a></p>
     </body>
   `);
 });
@@ -75,12 +79,19 @@ app.post("/handdle", async (req, res) => {
   try {
     let usr = await client.users.fetch(req.body.user.id);
     let hashesEq = '3f3d7f210a99c03b187c520552030420' === req.headers.authorization.replace('Bearer ','');
-    await usr.send(`Ты, __${req.body.user.username}__, купил __${req.body.text}__ с __uid ${req.body.uid}__.`+
+    await usr.send(`*Представь что ты не видел этого сообщения*`+`Ты, __${req.body.user.username}__, купил __${req.body.text}__ с __uid ${req.body.uid}__.`+
                    `Время: __${req.body.time}__\nТы потратил __${req.body.sum} АР__\n`+
                    `\`\`\`json\n${JSON.stringify(req.body, null, 2)}\n\`\`\`\n`+
                    `Хэш __${req.headers.authorization.replace('Bearer ','')}__`+
                    `${hashesEq ? '' : ' не'}`+
                    ` равен 3f3d7f210a99c03b187c520552030420 (хэши ${hashesEq ? '' : 'не '}совпадают и операция прошла ${hashesEq ? '' : 'не '}безопасно)`);
+    let d = new Date(req.body.time+(60000*60*5));
+    let em = new Discord.MessageEmbed().setTitle(`Заказ #${req.body.uid}`)
+                        .setDescription(`Здравствуйте, ${req.body.user.username},`+
+                                        ` вы заказали у нас **${req.body.text}** #${req.body.uid}\n`+
+                                        `Ваш заказ находится в ячейке ХХХ в ТФ, вы можете забрать`+
+                                        ` его до ${d.getHours()}:${d.getMinutes()}`).setColor("#346db3");
+    await usr.send(em);
   } catch (e) {console.log(e); console.log("FAIL :(");}
 });
 
